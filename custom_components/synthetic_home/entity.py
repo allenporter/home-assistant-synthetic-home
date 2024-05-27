9ab5d90bb545d8957e491170cc77f81ce0c4fb59
Allen Porter <allen.porter@gmail.com>
2024-05-27 18:29:10 +0000

    Expose all entities to the conversation agnet by default

--- custom_components/synthetic_home/entity.py
+++ custom_components/synthetic_home/entity.py
@@ -1,6 +1,8 @@
 """Base entity class for Synthetic Home."""
 
 from homeassistant.helpers.entity import Entity
+from homeassistant.components.conversation import DOMAIN as CONVERATION_DOMAIN
+from homeassistant.components.homeassistant.exposed_entities import async_expose_entity
 
 from .model import ParsedDevice
 
@@ -16,3 +18,9 @@ class SyntheticDeviceEntity(Entity):
         unique_id = device.unique_id
         self._attr_unique_id = f"{unique_id}-{key}"
         self._attr_device_info = device.device_info
+
+    async def async_added_to_hass(self) -> None:
+        """When entity is added to Home Assistant."""
+        await super().async_added_to_hass()
+        # Expose all synthetic home entities by default
+        async_expose_entity(self.hass, CONVERATION_DOMAIN, self.entity_id, True)
