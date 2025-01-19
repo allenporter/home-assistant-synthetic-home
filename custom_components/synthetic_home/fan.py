2e795cc8695bbadc0d4625c38e3d347beca181be
Allen Porter <allen.porter@gmail.com>
2025-01-19 01:54:57 +0000

    Set FanEntityFeature TURN_ON/OFF always

--- custom_components/synthetic_home/fan.py
+++ custom_components/synthetic_home/fan.py
@@ -73,7 +73,12 @@ class SyntheticFan(SyntheticEntity, FanEntity):
         """Initialize the SyntheticFan."""
         super().__init__(entity)
         if supported_features is not None:
-            self._attr_supported_features = FanEntityFeature(0) | supported_features
+            self._attr_supported_features = (
+                FanEntityFeature(0)
+                | FanEntityFeature.TURN_ON
+                | FanEntityFeature.TURN_OFF
+                | supported_features
+            )
         if state is not None:
             self._attr_percentage = 100 if (state == "on") else 0
         elif is_on is not None:
